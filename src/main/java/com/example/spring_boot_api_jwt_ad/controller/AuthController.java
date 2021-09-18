69e128bc5878f3f26a52c5b922c4c89b0719dd92
Nguyễn Trọng Tiến <nguyentrongtiencntt@gmail.com>
2021-09-18 08:02:25 +0700

    add add util pakege and JwtUltil class

--- src/main/java/com/example/spring_boot_api_jwt_ad/controller/AuthController.java
+++ src/main/java/com/example/spring_boot_api_jwt_ad/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.example.spring_boot_api_jwt_ad.controller;
 
+import com.example.spring_boot_api_jwt_ad.authen.UserPrincipal;
 import com.example.spring_boot_api_jwt_ad.entity.User;
 import com.example.spring_boot_api_jwt_ad.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,15 @@ public class AuthController {
     @PostMapping("/register")
     public User register(@RequestBody User user){
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+
+
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            UserPrincipal userPrincipal = (UserPrincipal) principal;
+        }
+
         return userService.createUser(user);
     }
 }
---
+++ src/main/java/com/example/spring_boot_api_jwt_ad/util/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.example.spring_boot_api_jwt_ad.util;
+
+import com.example.spring_boot_api_jwt_ad.authen.UserPrincipal;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+
+public class JwtUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+    private static final String USER = "tien";
+    private static final String SECRET = "no_reveal_this_key";
+
+    public String generateToken(UserPrincipal user) {
+        String token = null;
+        try {
+            JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder();
+
+            builder.claim(USER, user);
+            builder.expirationTime(generateExpirationDate());
+            JWTClaimsSet claimsSet = builder.build();
+
+            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+            JWSSigner signer = new MACSigner(SECRET.getBytes());
+            signedJWT.sign(signer);
+
+            token = signedJWT.serialize();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return token;
+    }
+
+    private Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis() + 864000000);
+    }
+
+}
