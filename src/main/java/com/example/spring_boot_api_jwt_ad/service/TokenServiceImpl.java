75fabba9cb6fdc836b97babef0374846498e0986
Nguyễn Trọng Tiến <nguyentrongtiencntt@gmail.com>
2021-09-18 08:11:30 +0700

    add add TokenRepo, TokenService and TokenServiceImpl

---
+++ src/main/java/com/example/spring_boot_api_jwt_ad/service/TokenServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.spring_boot_api_jwt_ad.service;
+
+import com.example.spring_boot_api_jwt_ad.entity.Token;
+import com.example.spring_boot_api_jwt_ad.repository.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenServiceImpl implements TokenService {
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    public Token createToken(Token token) {
+        return tokenRepository.saveAndFlush(token);
+    }
+}
+
